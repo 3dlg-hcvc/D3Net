674926cdf49e42bbe82b19b49f93c3b953bc01c1
eamonn-zh <eamonn0223@gmail.com>
2023-01-16 20:30:03 -0800

    fast eval

--- scripts/eval.py
+++ scripts/eval.py
@@ -189,7 +189,7 @@ def eval_detection(cfg, dataloader, model):
 
 def eval_grounding(cfg, dataset, dataloader, model):
     # random seeds
-    seeds = [cfg.general.manual_seed] + [2 * i for i in range(cfg.eval.repeat - 1)]
+    # seeds = [cfg.general.manual_seed] + [2 * i for i in range(cfg.eval.repeat - 1)]
 
     # scanrefer++ support
     seeds = [cfg.general.manual_seed]
@@ -201,11 +201,11 @@ def eval_grounding(cfg, dataset, dataloader, model):
     pred_path = os.path.join(cfg.general.root, "predictions.p")
     gen_flag = (not os.path.exists(score_path)) or cfg.eval.repeat > 1 or cfg.eval.force
     if gen_flag:
-        ref_acc_all = []
-        ious_all = []
-        masks_all = []
-        others_all = []
-        lang_acc_all = []
+        # ref_acc_all = []
+        # ious_all = []
+        # masks_all = []
+        # others_all = []
+        # lang_acc_all = []
         for seed in seeds:
             # reproducibility
             torch.manual_seed(seed)
@@ -218,11 +218,11 @@ def eval_grounding(cfg, dataset, dataloader, model):
             mem_hash = {}
 
             print("generating the scores for seed {}...".format(seed))
-            ref_acc = []
-            ious = []
-            masks = []
-            others = []
-            lang_acc = []
+            # ref_acc = []
+            # ious = []
+            # masks = []
+            # others = []
+            # lang_acc = []
             predictions = {}
             for data_dict in tqdm(dataloader):
 
@@ -260,11 +260,11 @@ def eval_grounding(cfg, dataset, dataloader, model):
                     mem_hash=mem_hash  # scanrefer++ support
                 )
 
-                ref_acc += data_dict["ref_acc"]
-                ious += data_dict["ref_iou"].cpu().numpy().tolist()
-                masks += data_dict["ref_multiple_mask"]
-                others += data_dict["ref_others_mask"]
-                lang_acc.append(data_dict["lang_acc"].item())
+                #ref_acc += data_dict["ref_acc"]
+                #ious += data_dict["ref_iou"].cpu().numpy().tolist()
+                #masks += data_dict["ref_multiple_mask"]
+                #others += data_dict["ref_others_mask"]
+                #lang_acc.append(data_dict["lang_acc"].item())
 
                 # store predictions
                 batch_ids = data_dict["id"]
@@ -315,29 +315,29 @@ def eval_grounding(cfg, dataset, dataloader, model):
                 # end
 
             # save to global
-            ref_acc_all.append(ref_acc)
-            ious_all.append(ious)
-            masks_all.append(masks)
-            others_all.append(others)
-            lang_acc_all.append(lang_acc)
+            # ref_acc_all.append(ref_acc)
+            # ious_all.append(ious)
+            # masks_all.append(masks)
+            # others_all.append(others)
+            # lang_acc_all.append(lang_acc)
 
         # convert to numpy array
-        ref_acc = np.array(ref_acc_all)
-        ious = np.array(ious_all)
-        masks = np.array(masks_all)
-        others = np.array(others_all)
-        lang_acc = np.array(lang_acc_all)
+        # ref_acc = np.array(ref_acc_all)
+        # ious = np.array(ious_all)
+        # masks = np.array(masks_all)
+        # others = np.array(others_all)
+        # lang_acc = np.array(lang_acc_all)
 
-        # save the global scores
-        with open(score_path, "wb") as f:
-            scores = {
-                "ref_acc": ref_acc_all,
-                "ious": ious_all,
-                "masks": masks_all,
-                "others": others_all,
-                "lang_acc": lang_acc_all
-            }
-            pickle.dump(scores, f)
+        # # save the global scores
+        # with open(score_path, "wb") as f:
+        #     scores = {
+        #         "ref_acc": ref_acc_all,
+        #         "ious": ious_all,
+        #         "masks": masks_all,
+        #         "others": others_all,
+        #         "lang_acc": lang_acc_all
+        #     }
+        #     pickle.dump(scores, f)
 
     else:
         print("loading the scores...")
@@ -351,128 +351,128 @@ def eval_grounding(cfg, dataset, dataloader, model):
             others = np.array(scores["others"])
             lang_acc = np.array(scores["lang_acc"])
 
-    multiple_dict = {
-        "unique": 0,
-        "multiple": 1
-    }
-    others_dict = {
-        "not_in_others": 0,
-        "in_others": 1
-    }
-
-    # evaluation stats
-    stats = {k: np.sum(masks[0] == v) for k, v in multiple_dict.items()}
-    stats["overall"] = masks[0].shape[0]
-    stats = {}
-    for k, v in multiple_dict.items():
-        stats[k] = {}
-        for k_o, v_o in others_dict.items():
-            stats[k][k_o] = np.sum(np.logical_and(masks[0] == v, others[0] == v_o))
-
-        stats[k]["overall"] = np.sum(masks[0] == v)
-
-    stats["overall"] = {}
-    for k_o, v_o in others_dict.items():
-        stats["overall"][k_o] = np.sum(others[0] == v_o)
-    
-    stats["overall"]["overall"] = masks[0].shape[0]
-
-    # aggregate scores
-    scores = {}
-    for k, v in multiple_dict.items():
-        for k_o in others_dict.keys():
-            ref_accs, acc_025ious, acc_05ious = [], [], []
-            for i in range(masks.shape[0]):
-                running_ref_acc = np.mean(ref_acc[i][np.logical_and(masks[i] == multiple_dict[k], others[i] == others_dict[k_o])]) \
-                    if np.sum(np.logical_and(masks[i] == multiple_dict[k], others[i] == others_dict[k_o])) > 0 else 0
-                running_acc_025iou = ious[i][np.logical_and(np.logical_and(masks[i] == multiple_dict[k], others[i] == others_dict[k_o]), ious[i] >= 0.25)].shape[0] \
-                    / ious[i][np.logical_and(masks[i] == multiple_dict[k], others[i] == others_dict[k_o])].shape[0] \
-                    if np.sum(np.logical_and(masks[i] == multiple_dict[k], others[i] == others_dict[k_o])) > 0 else 0
-                running_acc_05iou = ious[i][np.logical_and(np.logical_and(masks[i] == multiple_dict[k], others[i] == others_dict[k_o]), ious[i] >= 0.5)].shape[0] \
-                    / ious[i][np.logical_and(masks[i] == multiple_dict[k], others[i] == others_dict[k_o])].shape[0] \
-                    if np.sum(np.logical_and(masks[i] == multiple_dict[k], others[i] == others_dict[k_o])) > 0 else 0
-
-                # store
-                ref_accs.append(running_ref_acc)
-                acc_025ious.append(running_acc_025iou)
-                acc_05ious.append(running_acc_05iou)
-
-            if k not in scores:
-                scores[k] = {k_o: {} for k_o in others_dict.keys()}
-
-            scores[k][k_o]["ref_acc"] = np.mean(ref_accs)
-            scores[k][k_o]["acc@0.25iou"] = np.mean(acc_025ious)
-            scores[k][k_o]["acc@0.5iou"] = np.mean(acc_05ious)
-
-        ref_accs, acc_025ious, acc_05ious = [], [], []
-        for i in range(masks.shape[0]):
-            running_ref_acc = np.mean(ref_acc[i][masks[i] == multiple_dict[k]]) if np.sum(masks[i] == multiple_dict[k]) > 0 else 0
-            running_acc_025iou = ious[i][np.logical_and(masks[i] == multiple_dict[k], ious[i] >= 0.25)].shape[0] \
-                / ious[i][masks[i] == multiple_dict[k]].shape[0] if np.sum(masks[i] == multiple_dict[k]) > 0 else 0
-            running_acc_05iou = ious[i][np.logical_and(masks[i] == multiple_dict[k], ious[i] >= 0.5)].shape[0] \
-                / ious[i][masks[i] == multiple_dict[k]].shape[0] if np.sum(masks[i] == multiple_dict[k]) > 0 else 0
-
-            # store
-            ref_accs.append(running_ref_acc)
-            acc_025ious.append(running_acc_025iou)
-            acc_05ious.append(running_acc_05iou)
-
-        scores[k]["overall"] = {}
-        scores[k]["overall"]["ref_acc"] = np.mean(ref_accs)
-        scores[k]["overall"]["acc@0.25iou"] = np.mean(acc_025ious)
-        scores[k]["overall"]["acc@0.5iou"] = np.mean(acc_05ious)
-
-    scores["overall"] = {}
-    for k_o in others_dict.keys():
-        ref_accs, acc_025ious, acc_05ious = [], [], []
-        for i in range(masks.shape[0]):
-            running_ref_acc = np.mean(ref_acc[i][others[i] == others_dict[k_o]]) if np.sum(others[i] == others_dict[k_o]) > 0 else 0
-            running_acc_025iou = ious[i][np.logical_and(others[i] == others_dict[k_o], ious[i] >= 0.25)].shape[0] \
-                / ious[i][others[i] == others_dict[k_o]].shape[0] if np.sum(others[i] == others_dict[k_o]) > 0 else 0
-            running_acc_05iou = ious[i][np.logical_and(others[i] == others_dict[k_o], ious[i] >= 0.5)].shape[0] \
-                / ious[i][others[i] == others_dict[k_o]].shape[0] if np.sum(others[i] == others_dict[k_o]) > 0 else 0
-
-            # store
-            ref_accs.append(running_ref_acc)
-            acc_025ious.append(running_acc_025iou)
-            acc_05ious.append(running_acc_05iou)
-
-        # aggregate
-        scores["overall"][k_o] = {}
-        scores["overall"][k_o]["ref_acc"] = np.mean(ref_accs)
-        scores["overall"][k_o]["acc@0.25iou"] = np.mean(acc_025ious)
-        scores["overall"][k_o]["acc@0.5iou"] = np.mean(acc_05ious)
-   
-    ref_accs, acc_025ious, acc_05ious = [], [], []
-    for i in range(masks.shape[0]):
-        running_ref_acc = np.mean(ref_acc[i])
-        running_acc_025iou = ious[i][ious[i] >= 0.25].shape[0] / ious[i].shape[0]
-        running_acc_05iou = ious[i][ious[i] >= 0.5].shape[0] / ious[i].shape[0]
-
-        # store
-        ref_accs.append(running_ref_acc)
-        acc_025ious.append(running_acc_025iou)
-        acc_05ious.append(running_acc_05iou)
-
-    # aggregate
-    scores["overall"]["overall"] = {}
-    scores["overall"]["overall"]["ref_acc"] = np.mean(ref_accs)
-    scores["overall"]["overall"]["acc@0.25iou"] = np.mean(acc_025ious)
-    scores["overall"]["overall"]["acc@0.5iou"] = np.mean(acc_05ious)
-
-    # report
-    print("\nstats:")
-    for k_s in stats.keys():
-        for k_o in stats[k_s].keys():
-            print("{} | {}: {}".format(k_s, k_o, stats[k_s][k_o]))
-
-    for k_s in scores.keys():
-        print("\n{}:".format(k_s))
-        for k_m in scores[k_s].keys():
-            for metric in scores[k_s][k_m].keys():
-                print("{} | {} | {}: {}".format(k_s, k_m, metric, scores[k_s][k_m][metric]))
-
-    print("\nlanguage classification accuracy: {}".format(np.mean(lang_acc)))
+    # multiple_dict = {
+    #     "unique": 0,
+    #     "multiple": 1
+    # }
+    # others_dict = {
+    #     "not_in_others": 0,
+    #     "in_others": 1
+    # }
+    #
+    # # evaluation stats
+    # stats = {k: np.sum(masks[0] == v) for k, v in multiple_dict.items()}
+    # stats["overall"] = masks[0].shape[0]
+    # stats = {}
+    # for k, v in multiple_dict.items():
+    #     stats[k] = {}
+    #     for k_o, v_o in others_dict.items():
+    #         stats[k][k_o] = np.sum(np.logical_and(masks[0] == v, others[0] == v_o))
+    #
+    #     stats[k]["overall"] = np.sum(masks[0] == v)
+    #
+    # stats["overall"] = {}
+    # for k_o, v_o in others_dict.items():
+    #     stats["overall"][k_o] = np.sum(others[0] == v_o)
+    #
+    # stats["overall"]["overall"] = masks[0].shape[0]
+    #
+    # # aggregate scores
+    # scores = {}
+    # for k, v in multiple_dict.items():
+    #     for k_o in others_dict.keys():
+    #         ref_accs, acc_025ious, acc_05ious = [], [], []
+    #         for i in range(masks.shape[0]):
+    #             running_ref_acc = np.mean(ref_acc[i][np.logical_and(masks[i] == multiple_dict[k], others[i] == others_dict[k_o])]) \
+    #                 if np.sum(np.logical_and(masks[i] == multiple_dict[k], others[i] == others_dict[k_o])) > 0 else 0
+    #             running_acc_025iou = ious[i][np.logical_and(np.logical_and(masks[i] == multiple_dict[k], others[i] == others_dict[k_o]), ious[i] >= 0.25)].shape[0] \
+    #                 / ious[i][np.logical_and(masks[i] == multiple_dict[k], others[i] == others_dict[k_o])].shape[0] \
+    #                 if np.sum(np.logical_and(masks[i] == multiple_dict[k], others[i] == others_dict[k_o])) > 0 else 0
+    #             running_acc_05iou = ious[i][np.logical_and(np.logical_and(masks[i] == multiple_dict[k], others[i] == others_dict[k_o]), ious[i] >= 0.5)].shape[0] \
+    #                 / ious[i][np.logical_and(masks[i] == multiple_dict[k], others[i] == others_dict[k_o])].shape[0] \
+    #                 if np.sum(np.logical_and(masks[i] == multiple_dict[k], others[i] == others_dict[k_o])) > 0 else 0
+    #
+    #             # store
+    #             ref_accs.append(running_ref_acc)
+    #             acc_025ious.append(running_acc_025iou)
+    #             acc_05ious.append(running_acc_05iou)
+    #
+    #         if k not in scores:
+    #             scores[k] = {k_o: {} for k_o in others_dict.keys()}
+    #
+    #         scores[k][k_o]["ref_acc"] = np.mean(ref_accs)
+    #         scores[k][k_o]["acc@0.25iou"] = np.mean(acc_025ious)
+    #         scores[k][k_o]["acc@0.5iou"] = np.mean(acc_05ious)
+    #
+    #     ref_accs, acc_025ious, acc_05ious = [], [], []
+    #     for i in range(masks.shape[0]):
+    #         running_ref_acc = np.mean(ref_acc[i][masks[i] == multiple_dict[k]]) if np.sum(masks[i] == multiple_dict[k]) > 0 else 0
+    #         running_acc_025iou = ious[i][np.logical_and(masks[i] == multiple_dict[k], ious[i] >= 0.25)].shape[0] \
+    #             / ious[i][masks[i] == multiple_dict[k]].shape[0] if np.sum(masks[i] == multiple_dict[k]) > 0 else 0
+    #         running_acc_05iou = ious[i][np.logical_and(masks[i] == multiple_dict[k], ious[i] >= 0.5)].shape[0] \
+    #             / ious[i][masks[i] == multiple_dict[k]].shape[0] if np.sum(masks[i] == multiple_dict[k]) > 0 else 0
+    #
+    #         # store
+    #         ref_accs.append(running_ref_acc)
+    #         acc_025ious.append(running_acc_025iou)
+    #         acc_05ious.append(running_acc_05iou)
+    #
+    #     scores[k]["overall"] = {}
+    #     scores[k]["overall"]["ref_acc"] = np.mean(ref_accs)
+    #     scores[k]["overall"]["acc@0.25iou"] = np.mean(acc_025ious)
+    #     scores[k]["overall"]["acc@0.5iou"] = np.mean(acc_05ious)
+    #
+    # scores["overall"] = {}
+    # for k_o in others_dict.keys():
+    #     ref_accs, acc_025ious, acc_05ious = [], [], []
+    #     for i in range(masks.shape[0]):
+    #         running_ref_acc = np.mean(ref_acc[i][others[i] == others_dict[k_o]]) if np.sum(others[i] == others_dict[k_o]) > 0 else 0
+    #         running_acc_025iou = ious[i][np.logical_and(others[i] == others_dict[k_o], ious[i] >= 0.25)].shape[0] \
+    #             / ious[i][others[i] == others_dict[k_o]].shape[0] if np.sum(others[i] == others_dict[k_o]) > 0 else 0
+    #         running_acc_05iou = ious[i][np.logical_and(others[i] == others_dict[k_o], ious[i] >= 0.5)].shape[0] \
+    #             / ious[i][others[i] == others_dict[k_o]].shape[0] if np.sum(others[i] == others_dict[k_o]) > 0 else 0
+    #
+    #         # store
+    #         ref_accs.append(running_ref_acc)
+    #         acc_025ious.append(running_acc_025iou)
+    #         acc_05ious.append(running_acc_05iou)
+    #
+    #     # aggregate
+    #     scores["overall"][k_o] = {}
+    #     scores["overall"][k_o]["ref_acc"] = np.mean(ref_accs)
+    #     scores["overall"][k_o]["acc@0.25iou"] = np.mean(acc_025ious)
+    #     scores["overall"][k_o]["acc@0.5iou"] = np.mean(acc_05ious)
+    #
+    # ref_accs, acc_025ious, acc_05ious = [], [], []
+    # for i in range(masks.shape[0]):
+    #     running_ref_acc = np.mean(ref_acc[i])
+    #     running_acc_025iou = ious[i][ious[i] >= 0.25].shape[0] / ious[i].shape[0]
+    #     running_acc_05iou = ious[i][ious[i] >= 0.5].shape[0] / ious[i].shape[0]
+    #
+    #     # store
+    #     ref_accs.append(running_ref_acc)
+    #     acc_025ious.append(running_acc_025iou)
+    #     acc_05ious.append(running_acc_05iou)
+    #
+    # # aggregate
+    # scores["overall"]["overall"] = {}
+    # scores["overall"]["overall"]["ref_acc"] = np.mean(ref_accs)
+    # scores["overall"]["overall"]["acc@0.25iou"] = np.mean(acc_025ious)
+    # scores["overall"]["overall"]["acc@0.5iou"] = np.mean(acc_05ious)
+    #
+    # # report
+    # print("\nstats:")
+    # for k_s in stats.keys():
+    #     for k_o in stats[k_s].keys():
+    #         print("{} | {}: {}".format(k_s, k_o, stats[k_s][k_o]))
+    #
+    # for k_s in scores.keys():
+    #     print("\n{}:".format(k_s))
+    #     for k_m in scores[k_s].keys():
+    #         for metric in scores[k_s][k_m].keys():
+    #             print("{} | {} | {}: {}".format(k_s, k_m, metric, scores[k_s][k_m][metric]))
+    #
+    # print("\nlanguage classification accuracy: {}".format(np.mean(lang_acc)))
 
 def eval_captioning(cfg, dataset, dataloader, model):
     min_ious = [0, 0.25, 0.5]
